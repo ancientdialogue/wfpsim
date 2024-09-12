a03035cd9ee05b88b41176cdd59cc7d6eb260cd8
Hutao <wangsheng.77th.director@gmail.com>
2024-09-12 09:01:53 -0600

    Update blurb to Xilonen and Mualani

--- ui/packages/wfp/src/Components/Simulator/Simulator.tsx
+++ ui/packages/wfp/src/Components/Simulator/Simulator.tsx
@@ -44,9 +44,8 @@ export function Simulator({
         them. The following are characters/weapons/artifacts currently not
         implemented in gcsim that we have added here:
         <ul className="list-disc pl-4">
-          <li>Arlecchino</li>
-          <li>Clorinde</li>
-          <li>Sethos</li>
+          <li>Xilonen</li>
+          <li>Mualani</li>
         </ul>
         <p className=" font-bold">
           See{' '}
