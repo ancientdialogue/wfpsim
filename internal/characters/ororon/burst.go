4356a1ce000f8099eb68c1250ef9c8156826f80e
Hutao <wangsheng.77th.director@gmail.com>
2024-11-07 22:55:09 -0700

    Initial ororon commit

---
+++ internal/characters/ororon/burst.go
@@ -0,0 +1,86 @@
+package ororon
+
+import (
+	"github.com/genshinsim/gcsim/internal/frames"
+	"github.com/genshinsim/gcsim/pkg/core/action"
+	"github.com/genshinsim/gcsim/pkg/core/attacks"
+	"github.com/genshinsim/gcsim/pkg/core/attributes"
+	"github.com/genshinsim/gcsim/pkg/core/combat"
+)
+
+var burstFrames []int
+
+const burstHitmark = 51 // Initial Hit
+
+func init() {
+	burstFrames = frames.InitAbilSlice(80) // Q -> CA
+	burstFrames[action.ActionAttack] = 78  // Q -> N1
+	burstFrames[action.ActionSkill] = 57   // Q -> E
+	burstFrames[action.ActionDash] = 58    // Q -> D
+	burstFrames[action.ActionJump] = 58    // Q -> J
+	burstFrames[action.ActionSwap] = 56    // Q -> Swap
+}
+
+func (c *char) Burst(p map[string]int) (action.Info, error) {
+	// first zap has no icd and hits everyone
+	ai := combat.AttackInfo{
+		ActorIndex: c.Index,
+		Abil:       "Ritual (Initial)",
+		AttackTag:  attacks.AttackTagElementalBurst,
+		ICDTag:     attacks.ICDTagNone,
+		ICDGroup:   attacks.ICDGroupDefault,
+		StrikeType: attacks.StrikeTypeDefault,
+		Element:    attributes.Electro,
+		Durability: 0,
+		Mult:       burstRitual[c.TalentLvlBurst()],
+	}
+	c.Core.QueueAttack(
+		ai,
+		combat.NewCircleHitOnTarget(c.Core.Combat.Player(), nil, 5),
+		burstHitmark,
+		burstHitmark,
+		c.makeC2cb(),
+	)
+
+	ai = combat.AttackInfo{
+		ActorIndex: c.Index,
+		Abil:       "Soundwave Collision (Tick)",
+		AttackTag:  attacks.AttackTagElementalBurst,
+		ICDTag:     attacks.ICDTagElementalBurst,
+		ICDGroup:   attacks.ICDGroupDefault,
+		StrikeType: attacks.StrikeTypeDefault,
+		Element:    attributes.Electro,
+		Durability: 25,
+		Mult:       burstSonic[c.TalentLvlBurst()],
+	}
+
+	var snap combat.Snapshot
+	c.Core.Tasks.Add(func() {
+		snap = c.Snapshot(&ai)
+	}, burstHitmark-1)
+
+	firstTick := 111 // first tick at 111
+	for i := 0; i <= 9*60; i += 60 {
+		progress := i + firstTick
+		c.Core.Tasks.Add(func() {
+			c.Core.QueueAttackWithSnap(
+				ai,
+				snap,
+				combat.NewCircleHitFanAngle(c.Core.Combat.Player(), c.Core.Combat.PrimaryTarget(), nil, 10, 30),
+				0,
+				c.makeC2cb(),
+			)
+		}, progress)
+	}
+	c.c2OnBurst()
+	c.c6OnBurst()
+	c.SetCDWithDelay(action.ActionBurst, 15*60, 0)
+	c.ConsumeEnergy(0)
+	c.c4EnergyRestore()
+	return action.Info{
+		Frames:          frames.NewAbilFunc(burstFrames),
+		AnimationLength: burstFrames[action.InvalidAction],
+		CanQueueAfter:   burstFrames[action.ActionSwap], // earliest cancel
+		State:           action.BurstState,
+	}, nil
+}
