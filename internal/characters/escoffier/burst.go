703c269ae0873b202bdb39323d79f6cfeda6a6fc
Hutao <wangsheng.77th.director@gmail.com>
2025-03-27 23:08:00 -0600

    Initial escoffier work. No Cons yet

---
+++ internal/characters/escoffier/burst.go
@@ -0,0 +1,63 @@
+package escoffier
+
+import (
+	"github.com/genshinsim/gcsim/internal/frames"
+	"github.com/genshinsim/gcsim/pkg/core/action"
+	"github.com/genshinsim/gcsim/pkg/core/attacks"
+	"github.com/genshinsim/gcsim/pkg/core/attributes"
+	"github.com/genshinsim/gcsim/pkg/core/combat"
+	"github.com/genshinsim/gcsim/pkg/core/info"
+)
+
+var burstFrames []int
+
+const (
+	initialHeal = 91 // depends on ping
+	hitmark     = 91
+)
+
+func init() {
+	burstFrames = frames.InitAbilSlice(61) // Q -> N1/Dash/Walk
+	burstFrames[action.ActionSkill] = 60
+	burstFrames[action.ActionJump] = 60
+	burstFrames[action.ActionSwap] = 59
+}
+
+func (c *char) Burst(p map[string]int) (action.Info, error) {
+	ai := combat.AttackInfo{
+		ActorIndex: c.Index,
+		Abil:       "Scoring Cuts",
+		AttackTag:  attacks.AttackTagElementalBurst,
+		ICDTag:     attacks.ICDTagNone,
+		ICDGroup:   attacks.ICDGroupDefault,
+		StrikeType: attacks.StrikeTypeDefault,
+		Element:    attributes.Cryo,
+		Durability: 25,
+		Mult:       burst[c.TalentLvlBurst()],
+	}
+	c.Core.QueueAttack(ai, combat.NewCircleHitOnTarget(c.Core.Combat.Player(), nil, 6), hitmark, hitmark, c.makeA4CB())
+
+	// initial heal
+	c.QueueCharTask(func() {
+		heal := burstHealFlat[c.TalentLvlBurst()] + burstHealPer[c.TalentLvlBurst()]*c.TotalAtk()
+		c.Core.Player.Heal(info.HealInfo{
+			Caller:  c.Index,
+			Target:  -1,
+			Message: "Scoring Cuts (Healing)",
+			Src:     heal,
+			Bonus:   c.Stat(attributes.Heal),
+		})
+	}, initialHeal)
+
+	c.QueueCharTask(c.a1, hitmark)
+
+	c.SetCD(action.ActionBurst, int(burstCD[c.TalentLvlBurst()])*60)
+	c.ConsumeEnergy(6)
+
+	return action.Info{
+		Frames:          frames.NewAbilFunc(burstFrames),
+		AnimationLength: burstFrames[action.InvalidAction],
+		CanQueueAfter:   burstFrames[action.ActionSwap], // earliest cancel
+		State:           action.BurstState,
+	}, nil
+}
---
+++ internal/characters/escoffier/escoffier.go
@@ -0,0 +1,48 @@
+package escoffier
+
+import (
+	tmpl "github.com/genshinsim/gcsim/internal/template/character"
+	"github.com/genshinsim/gcsim/pkg/core"
+	"github.com/genshinsim/gcsim/pkg/core/info"
+	"github.com/genshinsim/gcsim/pkg/core/keys"
+	"github.com/genshinsim/gcsim/pkg/core/player/character"
+	"github.com/genshinsim/gcsim/pkg/model"
+)
+
+func init() {
+	core.RegisterCharFunc(keys.Escoffier, NewChar)
+}
+
+type char struct {
+	*tmpl.Character
+	skillSrc         int
+	a1Src            int
+	a4HydroCryoCount int
+}
+
+func NewChar(s *core.Core, w *character.CharWrapper, _ info.CharacterProfile) error {
+	c := char{}
+	c.Character = tmpl.NewWithWrapper(s, w)
+	c.BurstCon = 3
+	c.SkillCon = 5
+
+	c.EnergyMax = burstEnergy[c.TalentLvlBurst()]
+	c.NormalHitNum = normalHitNum
+
+	w.Character = &c
+
+	return nil
+}
+
+func (c *char) Init() error {
+	c.a4Init()
+
+	return nil
+}
+
+func (c *char) AnimationStartDelay(k model.AnimationDelayKey) int {
+	if k == model.AnimationXingqiuN0StartDelay {
+		return 12
+	}
+	return c.Character.AnimationStartDelay(k)
+}
