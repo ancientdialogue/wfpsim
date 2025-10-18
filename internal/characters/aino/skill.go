4b6d3a4e47ffbee625b742615b11235e65f90113
Hutao <wangsheng.77th.director@gmail.com>
2025-10-17 19:24:00 -0600

    fix aino skill canqueueafter

--- internal/characters/aino/skill.go
+++ internal/characters/aino/skill.go
@@ -75,7 +75,7 @@ func (c *char) Skill(p map[string]int) (action.Info, error) {
 	return action.Info{
 		Frames:          frames.NewAbilFunc(skillFrames),
 		AnimationLength: skillFrames[action.InvalidAction],
-		CanQueueAfter:   skillFrames[action.ActionDash], // earliest cancel
+		CanQueueAfter:   skillFrames[action.ActionJump], // earliest cancel
 		State:           action.SkillState,
 	}, nil
 }
