21cb924953b83ac1fe7f7a45b53da1825502651c
Hutao <wangsheng.77th.director@gmail.com>
2025-10-28 18:06:46 -0600

    Updated blurb

--- ui/packages/wfp/src/Components/Simulator/Simulator.tsx
+++ ui/packages/wfp/src/Components/Simulator/Simulator.tsx
@@ -53,6 +53,7 @@ export function Simulator({
           <li>Albedo (Magic)</li>
           <li>Razor (Magic)</li>
           <li>Klee (Magic)</li>
+          <li>Venti (Magic)</li>
           <li>Durin (C0)</li>
         </ul>
         <p className=" font-bold">
