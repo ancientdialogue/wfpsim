5f9c68b34c629ac505639970f417faffde4e8add
77th-Funeral-Director <wangsheng.77th.director@gmail.com>
2026-08-19 15:40:35 -0600

    Fix commit URLs

--- ui/packages/ui/src/Pages/Viewer/Components/Overview/Metadata/Commit.tsx
+++ ui/packages/ui/src/Pages/Viewer/Components/Overview/Metadata/Commit.tsx
@@ -13,7 +13,7 @@ export const Commit = memo(({ commit }: Props) => {
   }
 
   const shortCommit = commit?.substring(0, 7);
-  const url = "https://github.com/genshinsim/gcsim/commits/" + commit;
+  const url = "https://github.com/ancientdialogue/wfpsim/commits/" + commit;
 
   return (
     <Tag large={true} intent="none" minimal={true}>
