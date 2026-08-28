9c92a52ce68be18e926adf6f9b1c51f0c1bfaa7c
77th-Funeral-Director <wangsheng.77th.director@gmail.com>
2026-08-27 21:30:15 -0600

    Remove embed description

--- ui/packages/wfpworkers/src/preview/handleInjectHead.ts
+++ ui/packages/wfpworkers/src/preview/handleInjectHead.ts
@@ -1,43 +1,5 @@
-const escapeMetaValue = (value = '') =>
-  value
-    .replace(/&/g, '&amp;')
-    .replace(/"/g, '&quot;')
-    .replace(/</g, '&lt;')
-    .replace(/>/g, '&gt;');
-
-async function getShareDescription(host, key, prefix) {
-  const url = '/api/share/' + (prefix === 'db' ? 'db/' : '') + key;
-  try {
-    const res = await fetch(host + url);
-    if (!res.ok) {
-      return 'share not found';
-    }
-    const data = await res.json();
-    if (!data) {
-      return 'share no data';
-    }
-    const names = (data.character_details ?? [])
-      .map((character) => character?.name)
-      .filter((name) => typeof name === 'string' && name.trim().length > 0)
-      .slice(0, 4);
-    return names.length > 0 ? names.join(', ') : 'share data malformed';
-  } catch (_) {
-    return 'share error';
-  }
-}
-
 class ElementHandler {
-  private key;
-  private host;
-  private prefix;
-  private description;
-
-  constructor(host, key, prefix, description) {
-    this.key = key;
-    this.host = host;
-    this.prefix = prefix;
-    this.description = description;
-  }
+  constructor() {}
 
   element(element) {
     // An incoming element, such as `div`
@@ -58,7 +20,7 @@ class ElementHandler {
     element.append(
       `<meta
         property="og:description"
-        content="${escapeMetaValue(this.description)}"
+        content=""
     />`,
       {html: true},
     );
@@ -78,13 +40,9 @@ export async function handleInjectHead(request): Promise<Response> {
   const url = new URL(request.url);
   const segments = url.pathname.split('/');
   const key = segments.pop() || segments.pop();
-  const host = url.protocol + '//' + url.host;
-  const description = await getShareDescription(host, key, '');
   console.log('received share request: ' + key);
 
-  return new HTMLRewriter()
-    .on('head', new ElementHandler(host, key, '', description))
-    .transform(res);
+  return new HTMLRewriter().on('head', new ElementHandler()).transform(res);
 }
 
 export async function handleInjectHeadDB(request): Promise<Response> {
@@ -92,11 +50,7 @@ export async function handleInjectHeadDB(request): Promise<Response> {
   const url = new URL(request.url);
   const segments = url.pathname.split('/');
   const key = segments.pop() || segments.pop();
-  const host = url.protocol + '//' + url.host;
-  const description = await getShareDescription(host, key, 'db');
   console.log('received share request: ' + key);
 
-  return new HTMLRewriter()
-    .on('head', new ElementHandler(host, key, 'db', description))
-    .transform(res);
+  return new HTMLRewriter().on('head', new ElementHandler()).transform(res);
 }
