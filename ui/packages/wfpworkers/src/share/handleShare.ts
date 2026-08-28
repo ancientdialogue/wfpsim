d53a2e3ffebd0b38b8c482aef110632b5bae43a4
77th-Funeral-Director <wangsheng.77th.director@gmail.com>
2026-08-28 15:52:54 -0600

    Update share link duration to 180 days. Add char names to share link

--- ui/packages/wfpworkers/src/share/handleShare.ts
+++ ui/packages/wfpworkers/src/share/handleShare.ts
@@ -3,6 +3,18 @@ import {IRequest} from 'itty-router';
 import pako from 'pako';
 import {validator} from './validation';
 
+function getCharNames(data) {
+  if (!data) {
+    return '';
+  }
+  const names = (data.character_details ?? [])
+    .map((character) => character?.name.slice(0, 12))
+    .filter((name) => typeof name === 'string' && name.trim().length > 0)
+    .slice(0, 4);
+  console.log(names);
+  return names.length > 0 ? names.join('-') + '-' : '';
+}
+
 export async function handleShare(request: IRequest): Promise<Response> {
   let content: any;
   console.log('share request received! processing data');
@@ -25,11 +37,13 @@ export async function handleShare(request: IRequest): Promise<Response> {
 
   //save to kv
   try {
-    const key = uuid();
+    const charNamePrefix = getCharNames(content);
+    console.log(charNamePrefix);
+    const key = charNamePrefix + uuid();
     const data = pako.deflate(JSON.stringify(content));
     await WFPSIM_KV.put(key, data.buffer, {
-      expirationTtl: 60 * 60 * 24 * 31,
-    }); //7 days
+      expirationTtl: 60 * 60 * 24 * 180,
+    }); //180 days
     return new Response(key, {status: 200});
   } catch (err) {
     console.error(`KV returned error: ${err}`);
