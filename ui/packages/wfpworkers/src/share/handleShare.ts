fa879d613b8338cd90f0a9a2ebe7377a2383d1f2
77th-Funeral-Director <166459372+77th-Funeral-Director@users.noreply.github.com>
2026-08-28 22:43:57 -0600

    Update share link duration to 180 days. Add char names to share link (#195)
    * Update share link duration to 180 days. Add char names to share link
    
    * Make names sorted in alphabetical order

--- ui/packages/wfpworkers/src/share/handleShare.ts
+++ ui/packages/wfpworkers/src/share/handleShare.ts
@@ -3,6 +3,19 @@ import {IRequest} from 'itty-router';
 import pako from 'pako';
 import {validator} from './validation';
 
+function getCharNames(data) {
+  if (!data) {
+    return '';
+  }
+  const names = (data.character_details ?? [])
+    .map((character) => character?.name.slice(0, 12))
+    .filter((name) => typeof name === 'string' && name.trim().length > 0)
+    .slice(0, 4);
+  console.log(names);
+  const sortedNames = names.toSorted((a, b) => a.localeCompare(b));
+  return sortedNames.length > 0 ? sortedNames.join('-') + '-' : '';
+}
+
 export async function handleShare(request: IRequest): Promise<Response> {
   let content: any;
   console.log('share request received! processing data');
@@ -25,11 +38,13 @@ export async function handleShare(request: IRequest): Promise<Response> {
 
   //save to kv
   try {
-    const key = uuid();
+    const charNamePrefix = getCharNames(content);
+    console.log(charNamePrefix);
+    const key = charNamePrefix + uuid();
     const data = pako.deflate(JSON.stringify(content));
     await WFPSIM_KV.put(key, data.buffer, {
-      expirationTtl: 60 * 60 * 24 * 31,
-    }); //7 days
+      expirationTtl: 60 * 60 * 24 * 180,
+    }); //180 days
     return new Response(key, {status: 200});
   } catch (err) {
     console.error(`KV returned error: ${err}`);
